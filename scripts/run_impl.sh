397a2d721fdfa2ef78abd458117abbe2de7d21a7
Giovanni Silva <giovanni@atende.info>
2015-02-27 22:29:35 -0300

    fix execution of software

--- scripts/run_impl.sh
+++ scripts/run_impl.sh
@@ -8,7 +8,7 @@ TOMCAT_LOCATION=/opt/${SOFTWARE_NAME}/apache-tomcat
 start() {
   # Ensure the the volume home has the correct permissions, because this can cause errors
   chown ${SOFTWARE_NAME}.${SOFTWARE_NAME} -R /opt/${SOFTWARE_NAME}-home
-  exec /sbin/setuser ${SOFTWARE_NAME} -c /opt/${SOFTWARE_NAME}/start_${SOFTWARE_NAME}.sh
+  exec /sbin/setuser ${SOFTWARE_NAME} /opt/${SOFTWARE_NAME}/start_${SOFTWARE_NAME}.sh
 }
 post_start(){
   :
