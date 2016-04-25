ac5ea688b78db874021f2e0c480c8e16628c6b77
Giovanni Silva <giovanni@pucminas.br>
2016-04-25 18:29:31 -0300

    Jira software 7.1.2

--- scripts/install_impl.sh
+++ scripts/install_impl.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+install(){
+  echo "Downloading software ${SOFTWARE_NAME}"
+  curl -LSs https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-${SOFTWARE_VERSION}-jira-${SOFTWARE_VERSION}.tar.gz -o /tmp/${SOFTWARE_NAME}.tar.gz
+  echo "Installing ${SOFTWARE_NAME}"
+  mkdir -p /opt/${SOFTWARE_NAME}
+  tar zxf /tmp/${SOFTWARE_NAME}.tar.gz --strip=1 -C /opt/${SOFTWARE_NAME}
+  rm /tmp/${SOFTWARE_NAME}.tar.gz
+
+  useradd --create-home --home-dir /opt/${SOFTWARE_NAME}-home --shell /bin/bash ${SOFTWARE_NAME}
+
+  chown -R ${SOFTWARE_NAME}.${SOFTWARE_NAME} /opt/${SOFTWARE_NAME}
+
+  mkdir -p /opt/${SOFTWARE_NAME}-home
+}
+post_install(){
+  echo "${SOFTWARE_NAME}.home=/opt/${SOFTWARE_NAME}-home" > /opt/${SOFTWARE_NAME}/atlassian-${SOFTWARE_NAME}/WEB-INF/classes/${SOFTWARE_NAME}-application.properties
+}
