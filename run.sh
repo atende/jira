11606791b6fcfe75bcc9b8339b28354e6a9db144
Giovanni Silva <giovanni@atende.info>
2016-12-17 09:29:12 -0200

    running in foreground

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ pre_start_action() {
 start() {
   # Ensure the volume home has the correct permissions, because this can cause errors
   chown ${SOFTWARE_NAME}.${SOFTWARE_NAME} -R /opt/${SOFTWARE_NAME}-home
-  /sbin/setuser ${SOFTWARE_NAME} /opt/${SOFTWARE_NAME}/bin/start-${SOFTWARE_NAME}.sh
+  /sbin/setuser ${SOFTWARE_NAME} /opt/${SOFTWARE_NAME}/bin/catalina.sh run
 }
 post_start(){
   : # No Op
